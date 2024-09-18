8986f0c5c6c96a7b34cee4114959befd1c6a40b4
bielgirao <bielgirao@hotmail.com>
2024-09-18 19:03:17 -0300

    feat: add new endpoints to api

--- routes/api.php
+++ routes/api.php
@@ -2,33 +2,36 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::post('password/reset', 'App\Http\Controllers\AuthController@resetPassword');
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+
+    Route::name('api.')->group(function(){
+        Route::prefix('/tasks')->group(function(){
+            Route::get('/', [TaskController::class, 'index'])->name('index_task');
+            Route::get('/{id}', [TaskController::class, 'show'])->name('single_task');
+
+            Route::post('/', [TaskController::class, 'store'])->name('store_task');
+            Route::put('/{id}', [TaskController::class, 'update'])->name('update_task');
+            Route::patch('tasks/{id}/status', [TaskController::class, 'updateStatus']);
+
+            Route::delete('/{id}', [TaskController::class, 'delete'])->name('delete_task');
+        });
+
+        Route::prefix('/user')->group(function(){
+            Route::get('/{id}', [UserController::class, 'show']);
+            Route::put('/{id}', [UserController::class, 'update']);
+            Route::delete('/{id}', [UserController::class, 'delete']);
+        });
+    });
 });
 
-Route::post('login', 'App\Http\Controllers\AuthController@login');
-Route::post('register', 'App\Http\Controllers\AuthController@register');
-Route::post('logout', 'App\Http\Controllers\AuthController@logout')->middleware('auth:api');
-Route::post('password/reset', 'App\Http\Controllers\AuthController@resetPassword');
 
-Route::name('api.')->group(function(){
-    Route::prefix('/tasks')->group(function(){
-        Route::get('/', [App\Http\Controllers\TaskController::class, 'index'])->name('index_task');
-        Route::get('/{id}', [App\Http\Controllers\TaskController::class, 'show'])->name('single_task');
-
-        Route::post('/', [App\Http\Controllers\TaskController::class, 'store'])->name('store_task');
-        Route::put('/{id}', [App\Http\Controllers\TaskController::class, 'update'])->name('update_task');
-
-        Route::delete('/{id}', [App\Http\Controllers\TaskController::class, 'delete'])->name('delete_task');
-    });
-
-    Route::prefix('/user')->group(function(){
-        Route::get('/{id}', [App\Http\Controllers\UserController::class, 'show']);
-        Route::put('/{id}', [App\Http\Controllers\UserController::class, 'update']);
-        Route::delete('/{id}', [App\Http\Controllers\UserController::class, 'delete']);
-    });
-
-    Route::get('users', [App\Http\Controllers\UserController::class, 'index']);
-
-});
